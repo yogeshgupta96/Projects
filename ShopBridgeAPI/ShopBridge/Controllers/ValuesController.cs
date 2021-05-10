b055e450c1cd5bd63d1782878d7450308cff7dc8
nikhraswati <82213738+nikhraswati@users.noreply.github.com>
2021-05-10 15:52:57 +0530

    Uploaded API Project

---
+++ ShopBridgeAPI/ShopBridge/Controllers/ValuesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ShopBridge.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        
+        DataAccessLayer dal = new DataAccessLayer();
+        // GET api/values
+        [HttpGet]
+        public async Task<HttpResponseMessage> FetchProduct()
+        {
+            try
+            {
+                Task<List<Product>> task = new Task<List<Product>>(dal.GetProducts);
+                task.Start();
+                var result = await task;
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest,"");
+            }
+
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public async Task<HttpResponseMessage> FetchProductById(int id)
+        {
+            try
+            {
+                var result = await Task.FromResult<Product>(dal.GetProductById(id));
+            
+           
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch(Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "");
+
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<HttpResponseMessage> InsertProduct([FromBody]Product prod)
+        {
+            try
+            {
+              var a= await Task.FromResult<bool>(dal.AddProduct(prod));
+                return Request.CreateResponse(HttpStatusCode.OK, "Record inserted successfully");
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error occurred while inserting record");
+            }
+        }
+
+        [Route("api/Values/bulk")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> InsertProducts([FromBody]List<Product> list)
+        {
+            try
+            {
+                var a = await Task.FromResult<bool>(dal.BulkInsert(list));
+                return Request.CreateResponse(HttpStatusCode.OK, "Records have been inserted successfully");
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Error occurred while inserting record");
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public async Task<HttpResponseMessage> ModifyProuct(Product p)
+        {
+            try
+            {
+                var a = await Task.FromResult<bool>(dal.UpdateProdList(p));
+
+              return Request.CreateResponse(HttpStatusCode.OK, "Record updated successfully");
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "Error occurred while updating record");
+
+            }
+
+        }
+
+        [Route("api/Values/bulk")]
+        [HttpPut]
+        public async Task<HttpResponseMessage> ModifyProucts(List<Product> p)
+        {
+            try
+            {
+                var a = await Task.FromResult<bool>(dal.BulkUpdate(p));
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Records have been updated successfully");
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "Error occurred while updating the records");
+
+            }
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        public async Task<HttpResponseMessage> RemoveProduct(int id)
+        {
+            try
+            {
+                var a = await Task.FromResult<bool>(dal.DeleteProduct(id));
+
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Record deleted successfully");
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "Error occurred while deleting the record");
+            }
+        }
+        [HttpDelete]
+        [Route("api/Values/bulk")]
+        public async Task<HttpResponseMessage> RemoveProducts(List<Product> p)
+        {
+            try
+            {
+                var a = await Task.FromResult<bool>(dal.BulkDelete(p));
+
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Records have been deleted successfully");
+
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, "Error occurred while deleting the records");
+            }
+        }
+       
+    }
+}
---
+++ ShopBridgeAPI/ShopBridge/DataAccessLayer.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopBridge
+{
+    public class DataAccessLayer
+    {
+        
+      
+        ThinkBridgeDBEntities context = new ThinkBridgeDBEntities();
+
+        public List<Product> GetProducts()
+        {
+            try
+            {
+                var result = context.Products.ToList();
+                return result;
+            }
+            catch(Exception e)
+            {
+                throw (e);
+            }
+        }
+
+        public Product GetProductById(int id)
+        {
+            try
+            {
+                var result = context.Products.Where(x => x.ProdId == id).Single();
+                return result;
+            }
+            catch(Exception e)
+            {
+                throw (e);
+            }
+        }
+
+        public bool AddProduct(Product p)
+        {         
+            try
+            {
+                context.Products.Add(p);
+                context.SaveChanges();
+                return true;
+            }
+            catch(Exception e)
+            {
+                throw (e);
+            }
+
+        }
+        public bool BulkInsert(List<Product> p)
+        {
+            try
+            {
+                context.Products.BulkInsert(p);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+
+        }
+        public bool UpdateProdList(Product p)
+        {
+            try
+            {
+                var result = context.Products.Where(x => x.ProdId == p.ProdId).Single();
+                result.ProdId = p.ProdId;
+                result.Description = p.Description;
+                result.Price = p.Price;
+                result.Color = p.Color;
+                result.WarrantyPeriodInMonths = p.WarrantyPeriodInMonths;
+                context.SaveChanges();
+                return true;
+            }
+            catch(Exception e)
+            {
+                throw (e);
+            }
+
+        }
+        public bool BulkUpdate(List<Product> p)
+        {
+            try
+            {
+                context.Products.BulkUpdate(p);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+
+        }
+        public bool DeleteProduct(int id)
+        {
+            try
+            {
+                var result = context.Products.Where(x => x.ProdId == id).Single();
+                context.Products.Remove(result);
+                context.SaveChanges();
+                return true;
+            }
+            catch(Exception e)
+            {
+                throw (e);
+            }
+
+        }
+
+        public bool BulkDelete(List<Product> p)
+        {
+            try
+            {
+                context.Products.BulkDelete(p);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+
+        }
+
+    }
+}
+   
